47b844c5ed38037f9a9f5cbc56bec02aad2248b4
Sook-Hee Evans <evanssookhee@gmail.com>
2019-11-19 18:10:54 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 const express = require('express');
- const app = express();
- const path = require('path');
-
-//  app.get('/', function(req, res) {
-//      res.sendFile(path.join(__dirname, 'index.html'));
-//  });
+const app = express();
+const path = require('path');
 
  app.get('/', function(req, res) {
-     res.sendFile(path.join(__dirname, '/index.html'));
+     res.sendFile(path.join(__dirname, 'index.html'));
  });
 
  app.listen(process.env.PORT || 4000, function(){
