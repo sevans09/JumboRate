11af26f13e6b8015ad85ef239ca0b7db8d577c34
Sook-Hee Evans <evanssookhee@gmail.com>
2019-11-19 18:24:18 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,11 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const router = express.Router();
 
-//  app.get('/', function(req, res) {
-//      res.sendFile(path.join(__dirname, 'index.html'));
-//  });
+ app.get('/', function(req, res) {
+     res.sendFile(path.join(__dirname, 'index.html'));
+ });
 
-router.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'));
-});
-
-app.use('/', router);
 app.listen(process.env.port || 3000);
 
 console.log('Running at Port 3000');
