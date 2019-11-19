ef3024d51097b1880a04098a6f2b93c5e00fbdd8
Sook-Hee Evans <evanssookhee@gmail.com>
2019-11-19 18:29:14 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+app.use(express.static("public"));
 
  app.get('/', function(req, res) {
      res.sendFile(path.join(__dirname, 'index.html'));
