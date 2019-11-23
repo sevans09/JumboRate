874dc509109d707524b4be4300f1e9f007bab3f8
Nicole <nlkennedy7@gmail.com>
2019-11-23 12:52:05 -0500

    database stuff

--- database.js
+++ database.js
@@ -1,18 +1,31 @@
 
-// var database = firebase.database();
+// sample function
+// function writeUserData(userId, name, email) {
+//     console.log("user id: " + userId);
+// firebase.database().ref('users/' + userId).push({
+//     username: name,
+//     email: email
+//   }, function(error) {
+//     if (error) {
+//       // The write failed...
+//       console.log("the write failed");
+//     } else {
+//       // Data saved successfully!
+//       console.log("saved successfully");
+//     }
+//   });
+// }
 
-function writeUserData(userId, name, email) {
-    console.log("user id: " + userId);
-firebase.database().ref('users/' + userId).push({
-    username: name,
-    email: email
-  }, function(error) {
-    if (error) {
-      // The write failed...
-      console.log("the write failed");
-    } else {
-      // Data saved successfully!
-      console.log("saved successfully");
-    }
-  });
+function postClass(dep, class_num, info) {
+    firebase.database().ref('class/' + dep + '/' + class_num).push({
+        rating: info
+    }, function(error) {
+        if (error) {
+        // The write failed...
+        console.log("the write failed");
+        } else {
+        // Data saved successfully!
+        console.log("saved successfully");
+        }
+    });
 }
