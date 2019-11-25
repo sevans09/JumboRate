657dafde173eb758b7a15580b53162428a9ddfa9
Sook-Hee Evans <evanssookhee@gmail.com>
2019-11-25 15:07:53 -0500

    Update authentication.js

--- js/authentication/authentication.js
+++ js/authentication/authentication.js
@@ -1,8 +1,18 @@
 // Logs out current user
 function logOut() {
-  firebase.auth().signOut();
-  console.log('signed user out');
+	firebase.auth().signOut();
+	console.log('signed user out');
 }
+
+
+// Get the current logged in user (or null if no one logged in)
+// Use this function to decide to show log in or sign out
+function getUser() {
+	var user = firebase.auth().currentUser;
+	console.log('user is', user);
+	return user;
+}
+
 
 // Get all posts in the database. Update to take a field for dept and class 
 // number to allow filtering.
