798a1bf9ec5f17538f5002d0a45451a8d980c997
Nicole <nlkennedy7@gmail.com>
2019-11-26 14:14:32 -0500

    rate from search result

--- js/rate.js
+++ js/rate.js
@@ -14,18 +14,30 @@ function load_select_year() {
 
 function load_select_departments() {
     var xhr = new XMLHttpRequest();
-    
     xhr.onreadystatechange = function () {
         if (this.readyState != 4) return;
     
         if (this.status == 200) {
             data = JSON.parse(this.responseText);
             data.forEach(function(elem) {
-                $("#department").append(`<option value="` + elem + `">` + elem + `</option>`)
+                $("#dep").append(`<option value="` + elem + `">` + elem + `</option>`)
             });
+            load_preset_class();
         }
     };
 
     xhr.open('GET', '/departments_json', true);
     xhr.send();
+}
+
+// Only called if there are queries in the url
+function load_preset_class() {
+    if (location.search) {
+        var pairs = location.search.split('?')[1].split('&');
+        var dep = pairs[0].split('=')[1];
+        var class_num = pairs[1].split('=')[1];
+
+        document.getElementById("dep").value = dep;
+        document.getElementById("class_num").value = class_num;
+    }
 }
