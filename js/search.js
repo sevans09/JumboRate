502204d15d9cca910b228a5f2e9dcf038d72b6c7
Nicole <nlkennedy7@gmail.com>
2019-11-27 19:05:25 -0500

    comment & clean

--- js/search.js
+++ js/search.js
@@ -1,10 +1,15 @@
+// search.js 
+// JavaScript for search.ejs page
+
+// Global variable to hold database data
 var data = {};
 
 $(document).ready(function() {
-    get_data(update_class_dep, update_class_num);
+    get_data();
 }); 
 
-function get_data(update_class_num) {
+// Get all class rating data from database 
+function get_data() {
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
         if (this.readyState != 4) return;
@@ -19,25 +24,31 @@ function get_data(update_class_num) {
     xhr.send();
 }
 
+// Update the class department options dropdown based on available reviews 
+function update_class_dep() {
+    var classes = Object.keys(data);
+
+    // Update class department options 
+    $("#select_dep_num").html("");
+    classes.forEach(function(elem) {
+        $("#select_dep_num").append(`<option value="` + elem + `">` + elem+ `</option>`)
+    });
+
+    update_class_num();
+}
+
+// Update the class number options dropdown based on available reviews & department 
 function update_class_num() {
+    // Get the available class numbers based on current department
     var e = document.getElementById("select_dep_num");
     var selected = e.options[e.selectedIndex].text;
     var classes = Object.keys(data[selected]);
-    $("#select_class_num").html("");
 
+    // Update the class number options in dropdown 
+    $("#select_class_num").html("");
     classes.forEach(function(elem) {
         if (elem != 0) { 
             $("#select_class_num").append(`<option value="` + elem + `">` + elem+ `</option>`);
         }
     });
-}
-
-function update_class_dep() {
-    var classes = Object.keys(data);
-
-    $("#select_dep_num").html("");
-    classes.forEach(function(elem) {
-        $("#select_dep_num").append(`<option value="` + elem + `">` + elem+ `</option>`)
-    });
-    update_class_num();
 }
