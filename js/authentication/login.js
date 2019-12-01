6a7a5dd2b6ba6373f7ac403e7eb183e87234df26
Eliriana Lleshi <40076515+elilleshi@users.noreply.github.com>
2019-12-01 02:00:36 -0500

    Update login.js

--- js/authentication/login.js
+++ js/authentication/login.js
@@ -12,7 +12,7 @@ const uiConfig = {
     },
   },
   signInFlow: 'popup',
-  signInSuccessUrl: 'index.html',
+  signInSuccessUrl: 'index.ejs',
   signInOptions: [
     firebase.auth.EmailAuthProvider.PROVIDER_ID,
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
