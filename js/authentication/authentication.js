72b7977b2e4437cc489aec7504ef625c8bbbd51f
Nicole <nlkennedy7@gmail.com>
2019-12-08 15:56:25 -0500

    remove console logs

--- js/authentication/authentication.js
+++ js/authentication/authentication.js
@@ -4,13 +4,13 @@
 // Logs out current user
 function logOut() {
 	firebase.auth().signOut();
-	console.log('signed user out');
+	// console.log('signed user out');
 }
 
 // Get the current logged in user (or null if no one logged in)
 // Use this function to decide to show log in or sign out
 function getUser() {
 	var user = firebase.auth().currentUser;
-	console.log('user is', user);
+	// console.log('user is', user);
 	return user;
 }
